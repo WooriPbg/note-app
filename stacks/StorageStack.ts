042692e8680b71a965d068af226aa20e33118ac3
WooriPbg <wooripbg@gmail.com>
2022-03-25 16:25:41 +0900

    DynamoDB Table 생성

---
+++ stacks/StorageStack.ts
@@ -0,0 +1,23 @@
+import * as sst from "@serverless-stack/resources";
+
+export default class StorageStack extends sst.Stack {
+    public table: sst.Table;
+
+    constructor(scope: sst.App, id: string, props?: sst.StackProps) {
+        super(scope, id, props);
+
+        this.table = new sst.Table(this, "notes-pbg", {
+            dynamodbTable: {
+                tableName: 'nonets-pbg'
+            },
+            fields: {
+                userId: sst.TableFieldType.STRING,
+                noteId: sst.TableFieldType.STRING
+            },
+            primaryIndex: {
+                partitionKey: 'userId',
+                sortKey: 'noteId' 
+            }
+        });
+    }
+}
