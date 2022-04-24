762e884ee26336c925305fb453b6dc4218325d84
pbg <wooriPbg@gmail.com>
2022-04-24 21:30:49 +0900

    frontend base setting

--- stacks/index.ts
+++ stacks/index.ts
@@ -2,15 +2,23 @@ import * as sst from "@serverless-stack/resources";
 import StorageStack from "./StorageStack";
 import ApiStack from "./ApiStack";
 import AuthStack from "./AuthStack";
+import FrontendStack from './FrontendStack'
 
 export default function main(app: sst.App): void {
   const storageStack = new StorageStack(app, "storage-pbg");
+  
   const apiStack = new ApiStack(app, 'api-pbg', {
     table: storageStack.table
   });
 
-  new AuthStack(app, 'auth', {
+  const authStack = new AuthStack(app, 'auth', {
     api: apiStack.api,
+    bucket: storageStack.buket
+  });
+  
+  new FrontendStack(app, 'frontend', {
+    api: apiStack.api,
+    auth: authStack.auth,
     bucket: storageStack.buket
   });
 };
